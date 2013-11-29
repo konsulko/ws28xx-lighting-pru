70080d93f6adf50d4b0ba94adec52c1e6c4144b6
Matt Ranostay <mranostay@gmail.com>
2013-11-29 01:21:10 -0800

    Add initial PRU1 testing code
    Signed-off-by: Matt Ranostay <mranostay@gmail.com>

---
+++ src/ws28xx-pru1.asm
@@ -0,0 +1,92 @@
+;*
+;* PRU1 Application
+;*
+
+	.include "ws28xx-inc.asm"
+
+	.sect "text:main"
+	.global main
+
+main:
+	;* enable master OCP
+	LBCO &R0, CONST_PRUCFG, 4, 4
+	CLR R0, R0, 4
+	SBCO &R0, CONST_PRUCFG, 4, 4
+
+	;* assure we have blank shared memory
+	.asg R2,    OFFSET_REG
+	.asg R18,   DATA_REGS
+
+	LDI32 R0, CONST_SHARED_MEM
+	LDI UNIVERSE_COUNT, 0 ; universe counter
+$M1:
+	LDI SLOT_COUNT, 0     ; slot counter
+$M2:
+	;* zero memory
+	LDI32 R2, 0xFFFFFFFF; 0
+	SBBO &R2, R0, 0, 4
+
+	;* next slot
+	ADD SLOT_COUNT, SLOT_COUNT, 1
+	ADD R0, R0, 4
+	QBNE $M2, SLOT_COUNT, MAX_SLOTS
+
+	;* next universe
+	ADD UNIVERSE_COUNT, UNIVERSE_COUNT, 1
+	QBNE $M1, UNIVERSE_COUNT, MAX_UNIVERSES
+
+	;* Blank the LED strips now
+	JMP $M5
+	
+	;* Latch Data Out
+	;*
+$M3:
+	LDI R30.w0, 0         ; hold all lines low for 50 microseconds
+	DELAY_CYCLES 10000
+
+$M4:
+	;* Spin till we get an update interrupt from PRU0	
+	QBBC $M4, R31, 31
+$M5:
+	;* Clear interrupt
+	LDI R4.w2, 0
+	LDI R4.w0, PRU0_PRU1_INTERRUPT 
+	SBCO &R4, CONST_PRUSSINTC, SICR_OFFSET, 4 
+
+	LDI SLOT_COUNT, 0     ; slot counter
+	LDI32 OFFSET_REG, CONST_SHARED_MEM
+$M6:
+	; use 12 registers, one for each universe
+	LBBO &DATA_REGS, OFFSET_REG, 0, 4 * MAX_UNIVERSES
+
+	LDI BIT_COUNT, 0      ; bit counter
+$M7:
+	;* Assemble data for output
+	ASSEMBLE_DATA
+
+	;* Logic Low  -> high 0.40 uS -> low 0.85 uS (1.25 uS)
+	;* Logic High -> high 0.80 uS -> low 0.45 uS (1.25 uS)
+	;*
+
+	;* start for logic high + low
+	LDI R30.w0, 0x0FFF
+	DELAY_CYCLES 80 ; delay 0.40 uS
+
+	MOV R30.w0, R14.w0
+	DELAY_CYCLES 80 ; delay 0.40 uS
+
+	LDI R30.w0, 0x0000
+	DELAY_CYCLES 60 ; delay 0.45 uS - 32 clock cycles
+
+	;* shift everything left
+	SHIFT_DATA
+
+	ADD BIT_COUNT, BIT_COUNT, 1
+	QBNE $M7, BIT_COUNT, 24
+
+	;* next slot
+	ADD SLOT_COUNT, SLOT_COUNT, 1
+	ADD OFFSET_REG, OFFSET_REG, 4 * MAX_UNIVERSES
+	QBNE $M6, SLOT_COUNT, MAX_SLOTS
+
+	JMP $M3
