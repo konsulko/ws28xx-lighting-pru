b55283184432e60d570bf33091b3bfe45aaf20ba
Matt Ranostay <mranostay@gmail.com>
2013-12-28 02:37:38 -0800

    Fix rookie mistake bit swap errors...
    Signed-off-by: Matt Ranostay <mranostay@gmail.com>

--- src/ws28xx-inc.asm
+++ src/ws28xx-inc.asm
@@ -58,49 +58,42 @@ ASSEMBLE_DATA	.macro
 	LDI R14, 0
 
 
-	QBBC $1, R16, 0
+	QBBC $1, R16, 23
 	SET R14, R14, 0
 $1:
-	QBBC $2, R17, 0
+	QBBC $2, R17, 23
 	SET R14, R14, 1
 $2:
-	QBBC $3, R18, 0
+	QBBC $3, R18, 23
 	SET R14, R14, 2
 $3:
-	QBBC $4, R19, 0
+	QBBC $4, R19, 23
 	SET R14, R14, 3
 $4:
-	QBBC $5, R20, 0
+	QBBC $5, R20, 23
 	SET R14, R14, 4
 $5:
-	QBBC $6, R21, 0
+	QBBC $6, R21, 23
 	SET R14, R14, 5
 $6:
-	QBBC $7, R22, 0
+	QBBC $7, R22, 23
 	SET R14, R14, 6
 $7:
-	QBBC $8, R23, 0
+	QBBC $8, R23, 23
 	SET R14, R14, 7
 $8:
-	QBBC $9, R24, 0
+	QBBC $9, R24, 23
 	SET R14, R14, 8
 $9:
-	QBBC $10,R25, 0
+	QBBC $10,R25, 23
 	SET R14, R14, 9
 $10:
-	QBBC $11,R26, 0
+	QBBC $11,R26, 23
 	SET R14, R14, 10
 $11:
-	QBBC $12,R27, 0
+	QBBC $12,R27, 23
 	SET R14, R14, 11
 $12:
-;	QBBC $13,R28, 0
-;	SET R14, R14, 12
-;$13:
-;	QBBC $14,R29, 0 
-;	SET R14, R14, 13
-;$14:
-
 	.endm
 
 
@@ -109,21 +102,19 @@ SHIFT_DATA	.macro
 	;* 14 clock ticks
 
 	;* first 8 bits
-	LSR R16, R16, 1
-	LSR R17, R17, 1
-	LSR R18, R18, 1
-	LSR R19, R19, 1
-	LSR R20, R20, 1
-	LSR R21, R21, 1
-	LSR R22, R22, 1
-	LSR R23, R23, 1
+	LSL R16, R16, 1
+	LSL R17, R17, 1
+	LSL R18, R18, 1
+	LSL R19, R19, 1
+	LSL R20, R20, 1
+	LSL R21, R21, 1
+	LSL R22, R22, 1
+	LSL R23, R23, 1
 	
 	;* second 6 bits
-	LSR R24, R24, 1
-	LSR R25, R25, 1
-	LSR R26, R26, 1
-	LSR R27, R27, 1
-;	LSR R28, R28, 1
-;	LSR R29, R29, 1
+	LSL R24, R24, 1
+	LSL R25, R25, 1
+	LSL R26, R26, 1
+	LSL R27, R27, 1
 
 	.endm
